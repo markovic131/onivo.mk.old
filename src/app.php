e2ccd941eb4726332ac5c839a7d886bdad9a7323
Marko Aleksic <psybaron@gmail.com>
2014-03-31 17:33:56 +0200

    added 404 page, added terms and pp

--- src/app.php
+++ src/app.php
@@ -11,9 +11,29 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/views',
 ]);
 
+//Error Handlers
+$app->error(function (\Exception $e) use ($app) {
+
+    if ($e instanceof NotFoundHttpException) {
+
+        return $app['twig']->render('_errors/404.html.twig');
+
+    }
+
+    $code = ($e instanceof HttpException) ? $e->getStatusCode() : 500;
+
+    return $app['twig']->render('_errors/404.html.twig', ['code' => $code]);
+});
+
 //Routes
+$app->get('/terms', function() use($app) {
+    return $app['twig']->render('terms.html.twig');
+});
+
+$app->get('/privacy', function() use($app) {
+    return $app['twig']->render('privacy.html.twig');
+});
+
 $app->get('/', function() use($app) {
-
     return $app['twig']->render('index.html.twig');
-
 });
