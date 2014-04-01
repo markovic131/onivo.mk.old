c8dce698735569fd6b306b0db3f7f3254e3d64b9
Marko Aleksic <psybaron@gmail.com>
2014-04-01 09:36:01 +0200

    changed views file names

--- src/app.php
+++ src/app.php
@@ -16,24 +16,24 @@ $app->error(function (\Exception $e) use ($app) {
 
     if ($e instanceof NotFoundHttpException) {
 
-        return $app['twig']->render('_errors/404.html.twig');
+        return $app['twig']->render('_errors/404.twig');
 
     }
 
     $code = ($e instanceof HttpException) ? $e->getStatusCode() : 500;
 
-    return $app['twig']->render('_errors/404.html.twig', ['code' => $code]);
+    return $app['twig']->render('_errors/404.twig', ['code' => $code]);
 });
 
 //Routes
 $app->get('/terms', function() use($app) {
-    return $app['twig']->render('terms.html.twig');
+    return $app['twig']->render('terms.twig');
 });
 
 $app->get('/privacy', function() use($app) {
-    return $app['twig']->render('privacy.html.twig');
+    return $app['twig']->render('privacy.twig');
 });
 
 $app->get('/', function() use($app) {
-    return $app['twig']->render('index.html.twig');
+    return $app['twig']->render('index.twig');
 });
